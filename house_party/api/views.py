af4a6243a9efa2ff4c154c3af76c5cbe417463af
Edwin574 <owinoedwin574@gmail.com>
2023-12-05 23:21:10 +0300

    patch request

--- house_party/api/views.py
+++ house_party/api/views.py
@@ -123,9 +123,28 @@ class UpdateRoom(APIView):
     serializer_class=UpdateRoomSerializer
     
     def patch(self,request,format=None):
+        if not self.request.session.exists(self.request.session.session_key):
+            self.request.session.create()
         serializer=self.serializer_class(data=request.data)
         if serializer.is_valid():
-            pass
+            guest_can_pause=serializer.data.get('guest_can_pause')
+            votes_to_skip=serializer.data.get('votes_to_skip')
+            code=serializer.data.get('code')
+            
+            queryset=Room.objects.filter(code=code)
+            if not queryset.exists():
+                return Response({'msg':'Room not found'},status=status.HTTP_400_BAD_REQUEST)
+            room=queryset[0]
+            user_id=self.request.session.session_key
+            if room.room_host!=user_id:
+                return Response({'msg':'You are not the host of this room'},status=status.HTTP_403_FORBIDDEN)
+            
+            room.guest_can_pause=guest_can_pause
+            room.votes_to_skip=votes_to_skip
+            room.save(update_fields=['votes_to_skip','guest_can_pause'])
+            return Response(RoomSerializer(room).data,status=status.HTTP_200_OK)
+             
+                
         return Response({'Bad Request':'Invalid data...'},status=status.HTTP_400_BAD_REQUEST)
         
         
