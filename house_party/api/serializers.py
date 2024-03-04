516db42ee24a862fcdde105a65ef00826bfb67a9
Edwin574 <owinoedwin574@gmail.com>
2024-03-04 21:14:40 +0300

    modifying views

--- house_party/api/serializers.py
+++ house_party/api/serializers.py
@@ -3,29 +3,32 @@ from rest_framework import serializers
 from .models import Room
 
 
-
 class RoomSerializer(serializers.ModelSerializer):
-    '''
+    """
     handles outgoing response by representing the data in a better format.
-    '''
+    """
+
     class Meta:
-        model=Room
-        fields='__all__'
+        model = Room
+        fields = '__all__'
 
 
 class CreateRoomSerializer(serializers.ModelSerializer):
-    '''
-    :-handles incoming request and makes sure the posted payload is the same as what we expect
-    '''
+    """
+    -handles incoming request and makes sure the posted payload is the same as what we expect
+    """
+
     class Meta:
-        model=Room
-        fields=('guest_can_pause','votes_to_skip')
+        model = Room
+        fields = ('guest_can_pause', 'votes_to_skip')
+
 
 class UpdateRoomSerializer(serializers.ModelSerializer):
-    '''
-    :-handles incoming request and makes sure the posted payload is the same as what we expect
-    '''
-    code=serializers.CharField(validtors=[])
+    """
+    -handles incoming request and makes sure the posted payload is the same as what we expect
+    """
+    code = serializers.CharField(validtors=[])
+
     class Meta:
-        model=Room
-        fields=('guest_can_pause','votes_to_skip','code')
+        model = Room
+        fields = ('guest_can_pause', 'votes_to_skip', 'code')
